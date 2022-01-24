4929918bfec0dddf8f96c9c8c218ae088277e92d
kulym <kulym@LAPTOP-QSGCSTRJ>
2022-01-24 16:23:53 +0700

    Добавьте файлы проекта.

---
+++ Тюрьма/ДаWeed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Тюрьма
+{
+    class ДаWeed: Solocum, Duoсum
+    {
+      public void wank()
+        {
+            MessageBox.Show("Родился 1 апреля 1969г. в селе Иванино Московской области. " +
+                "До 14 лет периодически попадался на кражах женских трусиков. " +
+                "В 15 лет был поставлен на учёт за домагательство к маленькой девочке. " +
+                "В 18 лет попал за решётку за изнасилование (на удивление) несовершеннолетнего мальчика.");
+        }
+
+        public void FistingASS()
+        {
+            MessageBox.Show("");
+        }
+    }
+}
---
+++ Тюрьма/Судья.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Тюрьма
+{
+    class Судья 
+    {
+        Solocum Cum;
+        public Судья(Solocum C)
+        {
+            Cum = C;
+            MessageBox.Show("Вводите подсудимого");
+            Start();
+        }
+
+        void Start()
+        {
+            Cum.wank();
+        }
+    }
+}
